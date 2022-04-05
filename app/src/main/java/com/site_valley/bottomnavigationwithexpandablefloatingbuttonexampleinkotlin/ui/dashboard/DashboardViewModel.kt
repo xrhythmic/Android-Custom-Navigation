35e9b0d9f0c258b5ccbd62baba6455ef14a92baf
Jordan <jordprice23@gmail.com>
2022-04-05 20:55:32 +0100

    Initial Commit

--- app/src/main/java/com/site_valley/bottomnavigationwithexpandablefloatingbuttonexampleinkotlin/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/site_valley/bottomnavigationwithexpandablefloatingbuttonexampleinkotlin/ui/dashboard/DashboardViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class DashboardViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is List View Fragment"
     }
     val text: LiveData<String> = _text
 }
